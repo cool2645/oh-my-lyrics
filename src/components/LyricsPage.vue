bed66b940da77dac14ba7676d04fbeb67f1ea4ee
Riko Sakurauchi <lijiahao99131@gmail.com>
2019-11-19 21:09:35 +0800

    :heavy_plus_sign: codemirror

--- src/components/LyricsPage.vue
+++ src/components/LyricsPage.vue
@@ -1,36 +1,34 @@
 <template>
   <div class="editor-page">
     <p>每句歌词独占一行。空行表示间奏。</p>
-    <div class="editor" contenteditable="true" ref="editor"
-         @paste="onPaste" @input="onInput" spellcheck="false"
-    >
-    </div>
+    <div class="editor" ref="editor"></div>
     <div class="blank"></div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import CodeMirror, { Editor } from 'codemirror'
+import 'codemirror/lib/codemirror.css'
 
 export default Vue.extend({
+  data (): {
+    codeMirror: Editor | null
+    } {
+    return {
+      codeMirror: null
+    }
+  },
   mounted () {
-    (this.$refs.editor as HTMLElement).focus()
+    this.codeMirror = CodeMirror(this.$refs.editor as HTMLDivElement, {
+      value: '',
+      mode: null
+    })
+    this.codeMirror.on('change', (_, change) => {
+      console.log(change)
+    })
   },
   methods: {
-    onPaste (e: ClipboardEvent) {
-      e.preventDefault()
-      const text = e.clipboardData?.getData('text/plain')
-      if (!text) return
-      const lines = text.split('\n')
-      if (!lines || !lines.length) return
-      document.execCommand('insertText', false, lines.shift())
-      lines.forEach(line => {
-        document.execCommand('insertParagraph', false)
-        document.execCommand('insertText', false, line)
-      })
-    },
-    onInput (e: InputEvent) {
-    }
   }
 })
 </script>
@@ -41,14 +39,13 @@ export default Vue.extend({
 .blank
   width 100%
   height 60vh
-.editor
+.editor >>> .CodeMirror
   margin 20px 0
   padding 18px 24px
   border 1px solid lookup('$vue-ui-gray-100')
-  min-height 500px
+  height auto
   font-size 1.35rem
+  line-height 1.75em
   font-family serif
   outline 0
-  >>> p, >>> div
-    margin .35em 0
 </style>
